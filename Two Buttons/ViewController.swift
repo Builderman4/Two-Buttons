fa236d09289987a5391503386af7aaae485a2ae9
Builderman4 <ibrahim.syed5612@gmail.com>
2023-01-27 15:05:29 -0500

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by Ibrahim Syed on 2023-01-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
